3723bf19b633fd559d6fc9651f231ceb76726f1e
Alejandra Landa <98127121+aleLanda-rq@users.noreply.github.com>
2022-04-26 15:09:30 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,14 @@
 const baseUrl=('http://localhost:3000')
 fetch(baseUrl + '/movies')
-.then((res=>res.json()))
-.then((data=>imageList(data)))
+.then((res)=>res.json())
+.then((data)=>imageList(data))
 
-function imageList(movie){
+function imageList(movies){
 const cards=document.querySelector('.cards');
-const ummaImg=document.querySelector('#umma_image');
-ummaImg.src=movie.image;
-const outfitImg=document.createElement('img');
-outfitImg.setAttribute('id', 'outfitImage');
-outfitImg.src=movie.image;
-const contractorImg=document.createElement('img');
-contractorImg.setAttribute('id', 'contractorImage');
-contractorImg.src=movie.image;
-const nileImg=document.createElement('img');
-nileImg.setAttribute('id', 'nileImage');
-nileImg.src=movie.image;
-const moonfallImg=document.createElement('img');
-moonfallImg.setAttribute('id', 'moonfallImage');
-moonfallImg.src=movie.image;
-const infiniteImg=document.createElement('img');
-infiniteImg.setAttribute('id', 'infiniteImage');
-infiniteImg.src=movie.image;
-const aliceImg=document.createElement('img');
-aliceImg.setAttribute('id', 'aliceImage');
-aliceImg.src=movie.image
-cards.append(ummaImg, outfitImg,contractorImg, nileImg, moonfallImg, infiniteImg, aliceImg)
-}
+movies.forEach((movie)=>{
+    const image=document.createElement('img')
+    image.src=movie.image
+    image.setAttribute('class', 'image')
+    cards.append(image)
+})}
 
-fetch(baseUrl + '/movies')
-.then((res=>res.json()))
-.then((data=>audiobooks(data)))
-
-function audiobooks(audio){
-
-
-}
